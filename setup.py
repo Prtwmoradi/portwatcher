0139352f1959caa7babe9f72d859533b0ebad377
Prtwmoradi <perry.moradi@yahoo.com>
2025-03-05 22:39:28 +0100

    Made portwatcher a proper package

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="portwatcher",
+    name="portwatcher",  # Package name
     version="0.1",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[],  # Add dependencies here
     entry_points={
         "console_scripts": [
-            "portwatcher=portwatcher.main:main",
+            "portwatcher=portwatcher.main:main",  # Command-line usage
         ],
     },
 )
